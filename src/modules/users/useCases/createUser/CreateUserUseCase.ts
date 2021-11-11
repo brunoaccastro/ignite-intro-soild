38be6dc722a410334d2ec998409ee18e221f70a2
Bruno Castro <brunoacastro@gmail.com>
2021-11-10 21:58:46 -0400

    conclusao do desafio introdução dos conceitos de SOLID

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -9,8 +9,16 @@ interface IRequest {
 class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
+  execute({ name, email }: IRequest): User {
+    const emailAlreadyInUse = this.usersRepository.findByEmail(email);
+
+    if (emailAlreadyInUse) {
+      throw new Error("Email already in use!");
+    }
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
